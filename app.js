92756f20bc1676e380c5d3bed3430e5a656047f9
Zachary Hardin <hardinzw@gmail.com>
2019-07-02 21:03:37 -0400

    resolved conflict with app.js

--- app.js
+++ app.js
@@ -1,10 +1,2 @@
-    var queryURL = "https://api.discogs.com?api_key=GCsLSmSgsIIzroXNKCbsBdWNAsGiRgKScTDVGNqk"
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function(response){
-        console.log(response)
-    
 
-});
 
